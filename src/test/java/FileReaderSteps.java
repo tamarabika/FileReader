37854aacffe92137e847026182784ea10746b962
tamarabika <tamara.bikashova@gmail.com>
2023-03-15 20:38:43 +0100

    FileReader

--- src/test/java/FileReaderSteps.java
+++ src/test/java/FileReaderSteps.java
@@ -12,20 +12,49 @@ public class FileReaderSteps {
 
     private List<String> lastFiveLines;
 
-    @When("I read txt file")
-    public void iReadTxtFile() {
+    @When("I read a txt file with 6 lines")
+    public void iReadTxtFile6() {
         String[] sixLinesFile = {System.getProperty("user.dir") + "/src/test/resources/file6-lines.txt"};
+        lastFiveLines=FileReader.main(sixLinesFile);
+    }
+    @When("I read a text file with 5 lines")
+    public void iReadTxtFile5() {
+        String[] sixLinesFile = {System.getProperty("user.dir") + "/src/test/resources/file5-lines.txt"};
+        lastFiveLines=FileReader.main(sixLinesFile);
+    }
+    @When("I read a text file with less than 5 lines")
+    public void iReadTxtFile4() {
+        String[] sixLinesFile = {System.getProperty("user.dir") + "/src/test/resources/file4-lines.txt"};
         lastFiveLines=FileReader.main(sixLinesFile);
     }
 
     @Then("I see 5 last lines")
-    public void iSeeLastLines() {
+    public void iSeeLastLines6TXT() {
         List<String> expectedLines = new ArrayList<>();
         expectedLines.add("2LINE");
         expectedLines.add("3LINE");
         expectedLines.add("4LINE");
         expectedLines.add("5LINE");
         expectedLines.add("6LINE");
+        assertEquals(expectedLines, lastFiveLines);
+    }
+    @Then("I see all 5 lines")
+    public void iSeeLastLines5TXT() {
+        List<String> expectedLines = new ArrayList<>();
+        expectedLines.add("1LINE");
+        expectedLines.add("2LINE");
+        expectedLines.add("3LINE");
+        expectedLines.add("4LINE");
+        expectedLines.add("5LINE");
+        assertEquals(expectedLines, lastFiveLines);
+    }
+    @Then("I see less than 5 lines")
+    public void iSeeLastLines4TXT() {
+        List<String> expectedLines = new ArrayList<>();
+        expectedLines.add("1LINE");
+        expectedLines.add("2LINE");
+        expectedLines.add("3LINE");
+        expectedLines.add("4LINE");
         assertEquals(expectedLines, lastFiveLines);
     }
 }
--- src/test/resources/org/example/fileReader.feature
+++ src/test/resources/org/example/fileReader.feature
@@ -1,5 +1,15 @@
 Feature: Test File Reader
 
-  Scenario: Read 6 lines from .txt file
-    When I read txt file
+  Scenario: Read file with more than 5 lines
+    When I read a txt file with 6 lines
     Then I see 5 last lines
+
+
+  Scenario: Read file with 5 lines
+    When I read a text file with 5 lines
+    Then I see all 5 lines
+
+
+  Scenario: Read file with less than 5 lines
+    When I read a text file with less than 5 lines
+    Then I see less than 5 lines
