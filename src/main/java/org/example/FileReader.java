d9b4a283dd7d74fd5f64b0eedef1de0219d72643
tamarabika <tamara.bikashova@gmail.com>
2023-03-16 22:25:42 +0100

    FileReader

--- src/main/java/org/example/FileReader.java
+++ src/main/java/org/example/FileReader.java
@@ -1,11 +1,10 @@
 package org.example;
 
 import java.io.BufferedReader;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public  class FileReader {
+public class FileReader {
     public static List<String> main(String[] args) {
         if (args.length != 1) {
             System.err.println("Usage: java LastFiveLines <file>");
@@ -21,16 +20,20 @@ public  class FileReader {
                 lines.add(line);
                 line = reader.readLine();
             }
+
         } catch (Exception e) {
             System.err.println("Error reading file: " + e.getMessage());
             System.exit(1);
         }
 
         int startIndex = Math.max(0, lines.size() - 5);
-        for (int i = startIndex; i < lines.size(); i++) {
-            System.out.println(lines.get(i));
-            return lines.subList(startIndex, lines.size());
+        if (lines.size() < 5) {
+            System.err.println("File have less than 5 lines");
+        } else {
+            for (int i = startIndex; i < lines.size(); i++) {
+                System.out.println(lines.get(i));
+            }
         }
-        return lines;
+        return lines.subList(startIndex, lines.size());
     }
 }
