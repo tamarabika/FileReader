560221045a46c932a1eff814762efe0276a4110f
tamarabika <tamara.bikashova@gmail.com>
2023-03-19 13:40:03 +0100

    FileReader

--- src/main/java/org/example/FileReader.java
+++ src/main/java/org/example/FileReader.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class FileReader {
     public static List<String> main(String[] args) {
         if (args.length != 1) {
-            System.err.println("Usage: java LastFiveLines <file>");
+            System.err.println("Usage: java FileReader <file>");
             System.exit(1);
         }
 
@@ -27,13 +27,35 @@ public class FileReader {
         }
 
         int startIndex = Math.max(0, lines.size() - 5);
-        if (lines.size() < 5) {
-            System.err.println("File have less than 5 lines");
+
+        if (lines == null) {
+            System.err.println("Error: The file is empty.");
+        } else if (lines.size() < 5 && lines.size() > 0) {
+            System.err.println("File has less than 5 lines");
+        } else if (!isTxtFile(fileName)) {
+            System.err.println("Error: The file is not a text file.");
+        } else if (containsSpecialCharacters(lines)) {
+            System.err.println("Error: The file contains special characters.");
         } else {
             for (int i = startIndex; i < lines.size(); i++) {
                 System.out.println(lines.get(i));
             }
+
         }
         return lines.subList(startIndex, lines.size());
+    }
+
+    public static boolean isTxtFile(String fileName) {
+        String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
+        return extension.equals("txt");
+    }
+
+    public static boolean containsSpecialCharacters(List<String> lines) {
+        for (String line : lines) {
+            if (!line.matches("^[a-zA-Z0-9\\s]+$")) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/test/java/org/example/fileReader.feature
+++ src/test/java/org/example/fileReader.feature
@@ -13,3 +13,16 @@ Feature: Test File Reader
   Scenario: Read file with less than 5 lines
     When I read a text file with less than 5 lines
     Then I see error message
+
+  Scenario: Read empty file
+    When I read a text empty file
+    Then I see error empty file message
+
+  Scenario: Read img file
+    When I read a img file
+    Then I see error img file message
+
+
+  Scenario: Read file with special characters
+    When I read a txt file with special characters
+    Then I see error special characters file message
